cc44f2705a90c467ee72561be1a1142347e634d5
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-18 12:46:00 -0500

    saved the file

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ root.render(
       <Route path='/' element={<Home/>}/>
       <Route path='/myapps' element={<Navigate replace to="/learn"/>}/>
       <Route path='/learn' element={<Learn/>}>
-
+        
       </Route>
     </Routes>
   </Router>
