75e5cdb061157b873f21222d22eaa17108d1e82e
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-18 12:38:36 -0500

    importing Link to add links

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
+import {
+  BrowserRouter as Router, 
+  Routes, 
+  Route, 
+  Navigate,
+  Link
+} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,7 +16,7 @@ root.render(
   <Router>
     <Routes>
       <Route path='/' element={<Home/>}/>
-      <Route path='/myapps' element={<Navigate to="/learn"/>}/>
+      <Route path='/myapps' element={<Navigate replace to="/learn"/>}/>
       <Route path='/learn' element={<Learn/>}/>
     </Routes>
   </Router>
@@ -32,6 +38,8 @@ function Learn(){
       <h4>
         All courses are listed here
       </h4>
+      <Link to="/learn/course">courses</Link> ||
+      <Link to="/learn/bundle">bundle</Link>
     </div>
   )
 }
